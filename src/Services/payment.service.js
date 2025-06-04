5d66c452deb5c5261ce9402f5059acf97d766809
Sebastian_Juan <sjuacoc2t@adaits.es>
2025-06-04 17:38:52 +0200

    Update payment.service.js

--- src/Services/payment.service.js
+++ src/Services/payment.service.js
@@ -1,12 +1,15 @@
 import stripe from "../Utils/stripe.js";
 
 export const processStripePayment = async (amount, currency = "eur", paymentMethodId) => {
-    // Stripe espera el amount en céntimos
     const paymentIntent = await stripe.paymentIntents.create({
         amount: Math.round(amount * 100),
         currency,
         payment_method: paymentMethodId,
         confirm: true,
+        automatic_payment_methods: {
+            enabled: true,
+            allow_redirects: "never"
+        }
     });
     return paymentIntent;
 };
